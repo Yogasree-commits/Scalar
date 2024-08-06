aed3899ceaf91a49235f8932cc2eea9c13f3048b
Yogasree-commits <yogasreeu@gmail.com>
2024-08-06 19:43:29 +0530

    second commit

---
+++ src/main/java/org/example/package2/NewStudent.java
@@ -0,0 +1,22 @@
+package org.example.package2;
+
+public class NewStudent {
+    private int age;
+    public String name;
+    public NewStudent() {
+        this.age=23;
+        this.name="Yogasree";
+    }
+    public NewStudent(int age, String name) {
+        this.age=age;
+        this.name=name;
+    }
+    public static void main(String[] args) {
+        NewStudent newStudent=new NewStudent();
+        System.out.println(newStudent.age);
+        System.out.println(newStudent.name);
+        NewStudent newStudent2=new NewStudent(27,"Varadha");
+        System.out.println(newStudent2.age);
+        System.out.println(newStudent2.name);
+    }
+}
