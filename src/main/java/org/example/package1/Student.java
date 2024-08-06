c914c3dfdb472461e508f36199a9fed1a8de4b27
Yogasree-commits <yogasreeu@gmail.com>
2024-08-06 19:09:42 +0530

    first commit

---
+++ src/main/java/org/example/package1/Student.java
@@ -0,0 +1,14 @@
+package org.example.package1;
+
+public class Student {
+    String name;
+    int age;
+    String gender;
+    double psp;
+    public void display(Student s){
+        System.out.println("Name:"+s.name);
+        System.out.println("Age:"+s.age);
+        System.out.println("Gender:"+s.gender);
+        System.out.println("psp:"+s.psp);
+    }
+}
---
+++ src/main/java/org/example/package1/Test.java
@@ -0,0 +1,12 @@
+package org.example.package1;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.name="Varadha";
+        s.age=27;
+        s.gender="Male";
+        s.psp=100;
+        s.display(s);
+    }
+}
